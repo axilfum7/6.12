00760ca6c802b1c4d570770939081ac1534f2bf0
axilfum7 <muflixarahmatullayeva@gmail.com>
2025-07-24 12:01:35 +0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-// import React from 'react'
-// import Header from "./components/header/Header";
-
-// const App = () => {
-//   return (
-//     <div>
-//       <Header/>
-//     </div>
-//   )
-// }
-
-// export default App
-
 import SiteHeader from "./components/header/Header"
 import SiteHero from "./components/hero/Hero"
 import SiteFooter from "./components/footer/Footer"
 import SiteHeroAlt from "./components/heroSection/HeroSection"
 import SiteAwards from "./components/awards/AwardsSection"
-import "./globals.css"
 
 function App() {
   return (
